84364f51317143d712109d0e78752e3765cd0ee8
Iponeatdog <152945377+Iponeatdog@users.noreply.github.com>
2026-06-19 19:07:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
 from bs4 import BeautifulSoup
-# import lxml
 import requests
-
-# with open("website.html") as file:
-#     contents = file.read()
-
-# soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title)
-# print(soup.title.name)
-# print(soup.title.string)
-
-# print(soup.prettify())
-# print(soup.a)
-
-# all_anchor_tags = soup.find_all(name="a")
-# for tag in all_anchor_tags:
-#     print(tag.getText(), tag.get("href"))
-
-# print(soup.find(name="h1", id="name").getText())
-# print(soup.find(name="h3", class_="heading").getText())
-
-# company_url = soup.select_one(selector="p a")
-# print(company_url)
-#
-# name = soup.select_one(selector="#name")
-# print(name)
-#
-# headings = soup.select(".heading")
-# print(headings)
 
 response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
